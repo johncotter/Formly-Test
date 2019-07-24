3c61adcda307a148e49db5e9be231d90f92dbd9c
wildbohr <john.cotter@gmail.com>
2019-07-24 13:12:24 -0300

    Initial Test Code for Formly

--- apps/formtest/src/app/app.component.ts
+++ apps/formtest/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormlyFieldConfig} from '@ngx-formly/core';
 
 @Component({
   selector: 'formtest-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formtest';
+  form = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [{
+    key: 'email',
+    type: 'input',
+    templateOptions: {
+      label: 'Email address',
+      placeholder: 'Enter email',
+      required: true,
+    }
+  }];
+
+  submit(model) {
+    console.log(model);
+  }
 }
--- apps/formtest/src/app/app.module.ts
+++ apps/formtest/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormlyModule} from '@ngx-formly/core';
+import {FormlyMaterialModule} from '@ngx-formly/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, ReactiveFormsModule, FormlyModule.forRoot(), FormlyMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
